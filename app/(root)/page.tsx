09263c6048053457d6cba52755bd7bf85dafbfa6
shyam <shyammht3@gmail.com>
2025-07-16 11:07:16 +0530

    implement local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,66 @@
-import { auth, signOut } from '@/auth'
-import { Button } from '@/components/ui/button'
-import ROUTES from '@/constants/routes'
-import React from 'react'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
 
-const Home = async() => {
-    const session = await auth()
-    console.log(session)
+const questions = [
+    {
+        _id:"1",
+        title:"How to learn React?",
+        description:"I want to learn React?",
+        tags:[{
+            _id:"1",name:"React"
+        },{
+            _id:"2",name:"JavaScript"
+        }],
+        author:{_id:"1", name:"John Doe"},
+        upvotes:10,
+        answers:5,
+        views:100,
+        createdAt: new Date(),
+    },
+    {
+        _id:"2",
+        title:"How to learn JavaScript?",
+        description:"I want to learn JavaScript?",
+        tags:[{
+            _id:"1",name:"React"
+        },{
+            _id:"2",name:"JavaScript"
+        }],
+        author:{_id:"1", name:"John Doe"},
+        upvotes:10,
+        answers:5,
+        views:100,
+        createdAt: new Date(),
+    }
+]
+
+interface SearchParams{
+    searchParams:Promise<{[key:string]:string}>
+}
+
+const Home = async({searchParams}:SearchParams) => {
+    const {query=""} = await searchParams;
+    const filterdQuestions = questions.filter((q)=> q.title.toLowerCase().includes(query.toLowerCase()))
     return (
-        <h1 className="text-3xl font-bold underline text-primary-500">
-           <form className='px-10 pt-[100px]' action={async()=>{
-            'use server'
-            await signOut({redirectTo:ROUTES.SIGN_IN})
-           }}>
-                <Button type='submit'>Logout</Button>
-           </form>
-        </h1>
+       <>
+           <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+               <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+               <Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900" asChild>
+                   <Link href={ROUTES.ASK_QUESTION}>Ask a Questions</Link>
+               </Button>
+           </section>
+           <section className="mt-11">
+               <LocalSearch route="/" imgSrc='/icons/search.svg' placeholder='Search...' otherClasses="flex-1"/>
+           </section>
+           Home Filter
+           <div className="mt-10 flex flex-col w-full gap-6">
+               {filterdQuestions.map(question => (
+                   <h1 key={question._id}>{question.title}</h1>
+               ))}
+           </div>
+       </>
     )
 }
 export default Home
--- constants/routes.ts
+++ constants/routes.ts
@@ -3,7 +3,8 @@ const ROUTES = {
     SIGN_IN:'/sign-in',
     SIGN_UP:'/sign-up',
     PROFILE:(id:string)=> `/profile/${id}`,
-    TAGS:(id:string)=>`/tags/${id}`
+    TAGS:(id:string)=>`/tags/${id}`,
+    ASK_QUESTION:'/ask-question',
 }
 
 export default ROUTES;
